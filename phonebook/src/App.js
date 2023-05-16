b683c0395d01819de07bd4546e2ccd652b9a49cc
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-16 16:57:11 +0930

    Add function to filter through contact using input

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'artis hellas', key: 'artic hellas', number: 123123123 }
+    { name: 'artis hellas', key: 'artis hellas', number: 123123123 },
+    { name: 'daniel burgess', key: 'daniel burgess', number: 932495948 },
+    { name: 'terry terrison', key: 'terry terrison', number: 577438039 },
+    { name: 'blob flerg', key: 'blob flerg', number: 768430358 },
+    { name: 'swincy swashton', key: 'swinsy swashton', number: 437592302 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterText, setFilterText] = useState('')
 
   const addContact = (event) => {
     event.preventDefault()
@@ -30,9 +35,18 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilterText(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter contacts:
+        <input value={filterText} onChange={(handleFilterChange)} />
+      </div>
+      <h2>Add new</h2>
       <form
         onSubmit={
           persons.map((person) => person.name).includes(newName)
@@ -51,9 +65,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => {
-        return <p key={person.key}> {person.name} - {person.number}</p>
-      })}
+      {persons
+        .filter((person) => person.name.includes(filterText))
+        .map(person => (
+          <p key={person.key}>{person.name} - {person.number} </p>
+        ))}
     </div>
   )
 }
