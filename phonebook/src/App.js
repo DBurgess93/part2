2948bbf2de9be2fca9d38546d27fd446c5bf42b7
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-16 15:12:04 +0930

    Add form to add new contacts into phonebook and display them

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react'
 
 const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      key: newName
+    }
+    console.log(nameObject)
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
-      <h1>Phonebook</h1>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName} >
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person => {
+          return <p key={person.key}> {person.name}</p>
+        })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
