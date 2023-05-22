0a30aae88a4ffeb568e836df9bec16789482560d
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-22 23:14:45 +0930

    add importance toggle button and functioanlity to update server

--- notes/src/App.js
+++ notes/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [newNote, setNewNote] = useState('a new note..')
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
+  useEffect(() => {
     console.log('effect')
     axios
       .get('http://localhost:3001/notes')
@@ -15,9 +15,8 @@ const App = () => {
         console.log('promise fulfilled')
         setNotes(response.data)
       })
-  }
-  useEffect(hook, [])
-  
+  }, [])
+
   console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
@@ -25,16 +24,30 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf = id => {
+    console.log(`importance of ${id} needs to be toggled`)
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote= { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n :response.data))
+    })
   }
 
   const notesToShow = showAll
@@ -51,7 +64,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote} >
